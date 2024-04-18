fe470506f55a996a2e412776c102b98aa0d5c4b2
aphremthomas <aphremthomas@gmail.com>
2024-04-18 00:18:54 -0400

    contact form added

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Contact() {
   return (
     <main className="bg-red text-white">
-      howdi partner...
+     <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfa0GiSysBc0HMfYDgzo0oCwzHcEJge-8lcgIkzNo8YCkG6XQ/viewform?embedded=true" width="640" height="852">Loading…</iframe>
     </main>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="w-full md:container">
-      <div className="hello p-2 flex flex-col h-[75vh] items-center justify-center md:flex-row">
+    <main className="w-full flex flex-col items-center">
+      <div className="hello p-2 flex flex-col h-[75vh] items-center justify-center md:flex-row md:container">
         <div className="profileAndWelcome flex-col">
           <div className="pic rounded-xl">
             <img className="rounded-full h-16" src="/aphrem.jpg" alt="" />
@@ -12,7 +12,7 @@ export default function Home() {
             Hello it&apos;s Aphrem Thomas
           </div>
         </div>
-        <div className="intro flex flex-col justify-between mt-6 h-40">
+        <div className="intro flex flex-col justify-between mt-6 h-40 md:ml-20">
           <div className="introheader text-2xl">
             Web Developer based in Ottawa Canada.
           </div>
@@ -20,11 +20,13 @@ export default function Home() {
               JavaScript | React | NodeJS Developer
           </div>
           <div className="connectButtons">
-            <button className="h-10 w-32 rounded-md bg-contrast-button-main text-background">Let&apos;s Connect</button>
+            <a href="/contact">
+              <button className="h-10 w-32 rounded-md bg-contrast-button-main text-background">Let&apos;s Connect</button>
+            </a>
           </div>
         </div>
       </div>
-      <div className="boasting bg-background-secondary h-96"></div>
+      <div className="boasting w-full bg-background-secondary h-96"></div>
     </main>
   );
 }
