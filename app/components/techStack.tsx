5f0dc1f59f7e754860f22725a530d3ab5bb90c86
aphremthomas <aphremthomas@gmail.com>
2024-06-12 19:03:12 -0400

    tech stack added

--- app/components/techStack.tsx
+++ app/components/techStack.tsx
@@ -14,7 +14,7 @@ export function TechStack() {
     <div className="techstacktitle text-2xl mb-4">Tech Stack</div>
     <div className="tech flex flex-wrap justify-between">
       {tech.map((item) => (
-        <div className="techwrapper bg-background rounded-md shadow-sm h-24 w-20 flex flex-col items-center justify-center mb-4" data-aos="fade-up">
+        <div key={item.title} className="techwrapper bg-background rounded-md shadow-sm h-24 w-20 flex flex-col items-center justify-center mb-4" data-aos="fade-up">
           <div className="image h-10 w-10">
             <img className="image h-10 w-10" src={item.image} alt="" />
           </div>
