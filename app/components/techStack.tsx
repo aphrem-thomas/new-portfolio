7fc6265b8e16697167f693612771ed5816cc2e4c
aphremthomas <aphremthomas@gmail.com>
2024-06-13 00:20:35 -0400

    monochromatic techstack added

--- app/components/techStack.tsx
+++ app/components/techStack.tsx
@@ -1,22 +1,22 @@
 export function TechStack() {
   const tech = [
-    { image: "css-3.svg", title: "css" },
-    { image: "docker-3.svg", title: "docker" },
-    { image: "golang-1.svg", title: "go" },
-    { image: "html-1.svg", title: "html" },
-    { image: "logo-javascript.svg", title: "JavaScript" },
-    { image: "next-js.svg", title: "Next JS" },
-    { image: "nodejs-3.svg", title: "Node JS" },
-    { image: "react-2.svg", title: "React JS" },
+    { image: "devicon-css3-plain", title: "css" },
+    { image: "devicon-docker-plain", title: "docker" },
+    { image: "devicon-go-original-wordmark", title: "go" },
+    { image: "devicon-html5-plain", title: "html" },
+    { image: "devicon-javascript-plain", title: "JavaScript" },
+    { image: "devicon-nextjs-plain colored", title: "Next JS" },
+    { image: "devicon-nodejs-plain-wordmark", title: "Node JS" },
+    { image: "devicon-react-original", title: "React JS" },
   ];
   return (
     <>
     <div className="techstacktitle text-2xl mb-4">Tech Stack</div>
     <div className="tech flex flex-wrap justify-between">
       {tech.map((item) => (
-        <div key={item.title} className="techwrapper bg-background rounded-md shadow-sm h-24 w-20 flex flex-col items-center justify-center mb-4" data-aos="fade-up">
+        <div key={item.title} className="techwrapper h-24 w-20 flex flex-col items-center justify-center mb-4" data-aos="fade-up">
           <div className="image h-10 w-10">
-            <img className="image h-10 w-10" src={item.image} alt="" />
+          <i className={`text-4xl ${item.image}`}></i>
           </div>
           <div className="techtitle text-center">{item.title}</div>
         </div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head><link rel="stylesheet" type='text/css' href="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css" /></head>
       <body className={inter.className}>{children}</body>
     </html>
   );
